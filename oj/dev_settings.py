2ca96ca228ee7c8f78605a63d34f77f4102b8832
yuetsh <517252939@qq.com>
2024-06-04 20:09:16 +0800

    update python deps

--- oj/dev_settings.py
+++ oj/dev_settings.py
@@ -5,19 +5,19 @@ from utils.shortcuts import get_env
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': get_env('POSTGRES_HOST', '127.0.0.1'),
-        'PORT': get_env('POSTGRES_PORT', '5435'),
-        'NAME': get_env('POSTGRES_DB', 'onlinejudge'),
-        'USER': get_env('POSTGRES_USER', 'onlinejudge'),
-        'PASSWORD': get_env('POSTGRES_PASSWORD', 'onlinejudge')
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "HOST": get_env("POSTGRES_HOST", "127.0.0.1"),
+        "PORT": get_env("POSTGRES_PORT", "5435"),
+        "NAME": get_env("POSTGRES_DB", "onlinejudge"),
+        "USER": get_env("POSTGRES_USER", "onlinejudge"),
+        "PASSWORD": get_env("POSTGRES_PASSWORD", "onlinejudge"),
     }
 }
 
 REDIS_CONF = {
-    'host': get_env('REDIS_HOST', '127.0.0.1'),
-    'port': get_env('REDIS_PORT', '6380')
+    "host": get_env("REDIS_HOST", "127.0.0.1"),
+    "port": get_env("REDIS_PORT", "6380"),
 }
 
 
