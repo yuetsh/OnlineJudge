6d32e5de1e6f46b11a7e963a27f942b7f77b928f
yuetsh <517252939@qq.com>
2025-10-05 16:43:54 +0800

    add sort

--- account/views/admin.py
+++ account/views/admin.py
@@ -3,7 +3,7 @@ import re
 import xlsxwriter
 
 from django.db import transaction, IntegrityError
-from django.db.models import Q
+from django.db.models import Q, F
 from django.http import HttpResponse
 from django.contrib.auth.hashers import make_password
 from django.utils.crypto import get_random_string
@@ -154,7 +154,19 @@ class UserAdminAPI(APIView):
                 return self.error("User does not exist")
             return self.success(UserAdminSerializer(user).data)
 
-        user = User.objects.all().order_by("-create_time")
+        # 获取排序参数
+        order_by = request.GET.get("order_by", "")
+        
+        # 根据排序参数设置排序规则
+        if order_by == "-last_login":
+            # 最近登录，将 None 值放在最后
+            user = User.objects.all().order_by(F("last_login").desc(nulls_last=True))
+        elif order_by == "last_login":
+            # 最早登录，将 None 值放在最后
+            user = User.objects.all().order_by(F("last_login").asc(nulls_last=True))
+        else:
+            # 默认按创建时间倒序
+            user = User.objects.all().order_by("-create_time")
 
         type = request.GET.get("type", "")
 
