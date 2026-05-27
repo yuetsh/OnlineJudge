c49a6af0664347c0a1e744c6a4375751881348b1
yuetsh <517252939@qq.com>
2026-05-26 21:25:48 -0600

    refactor: replace sync_to_async cache wrappers with native cache.aget/aset/adelete

--- utils/async_helpers.py
+++ utils/async_helpers.py
@@ -1,14 +1,13 @@
-from asgiref.sync import sync_to_async
 from django.core.cache import cache
 
 
 async def async_cache_get(key, default=None):
-    return await sync_to_async(cache.get, thread_sensitive=True)(key, default)
+    return await cache.aget(key, default)
 
 
 async def async_cache_set(key, value, timeout=None):
-    return await sync_to_async(cache.set, thread_sensitive=True)(key, value, timeout)
+    return await cache.aset(key, value, timeout)
 
 
 async def async_cache_delete(key):
-    return await sync_to_async(cache.delete, thread_sensitive=True)(key)
+    return await cache.adelete(key)
