fbbbe11388421126c035ec80d250b99e377cae69
yuetsh <517252939@qq.com>
2026-08-05 06:00:03 -0600

    feat: 后台题目列表支持按标签筛选

--- problem/views/admin.py
+++ problem/views/admin.py
@@ -276,6 +276,11 @@ class ProblemAPI(ProblemBase):
         keyword = request.GET.get("keyword", "").strip()
         if keyword:
             problems = problems.filter(Q(title__icontains=keyword) | Q(_id__icontains=keyword))
+
+        tag_id = request.GET.get("tag_id")
+        if tag_id:
+            problems = problems.filter(tags__id=tag_id)
+
         if not user.can_mgmt_all_problem():
             problems = problems.filter(created_by=user)
         return self.success(self.paginate_data(request, problems, ProblemAdminListSerializer))
